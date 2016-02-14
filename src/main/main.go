a62aebb0b25dacfec00d2d7e87b42fe1945b68a2
aarskog <eparis@atlassian.com>
2016-02-14 13:52:58 +0100

    sdf

--- src/main/main.go
+++ src/main/main.go
@@ -1,33 +1,22 @@
 package main
+
 //export GOPATH=$HOME/Documents/MagAnd/Heismappe/
 
 import (
-    "driver"
-    "time"
-    "asd"
-    "fmt"
+	"asd"
+	"driver"
+	//"fmt"
+	"time"
 )
 
-
-
-func main (){
+func main() {
 	driver.Elev_init()
-	var cur_floor int
-
-	c := make(chan int)
-	go func(){
-		fmt.Println("jfsdkgæø")
-		for{
-			if driver.Get_floor_sensor_signal() != -1{
-				cur_floor = driver.Get_floor_sensor_signal()
-					c <- cur_floor
-			}
-			time.Sleep(10*time.Millisecond)
-		}
-	}()
-
-	asd.Go_to_floor(2, c)
+	go asd.Update_lights()
+	go asd.Go_to_floor(2)
 	time.Sleep(time.Second)
+	go asd.Go_to_floor(1)
+	quit := make(chan int)
+	go asd.Quit_program(quit)
 
-
+	<-quit
 }
