17291338d357d376b3f5fefc8061f61b93210d5a
aarskog <eparis@atlassian.com>
2016-02-14 13:55:29 +0100

    sdf

--- src/asd/functions.go
+++ src/asd/functions.go
@@ -1,24 +1,58 @@
 package asd
 
-import(
+import (
 	"driver"
+	//"fmt"
 	"math"
-	"fmt"
+	"time"
 )
 
-func Go_to_floor(floor int, c chan int){
+func Go_to_floor(floor int) {
 	var dir float64
-	cur_floor:=<-c
-	dir = (float64(floor-cur_floor))/math.Abs(float64(floor-cur_floor))
+	var cur_floor int = driver.Get_floor_sensor_signal()
+	dir = (float64(floor - cur_floor)) / math.Abs(float64(floor-cur_floor))
 	driver.Set_motor_dir(int(dir))
-	//event som venter på at den kommer til en etashe 
-	for{
-		fmt.Println("hey")
-		cur_floor:=<-c
-		fmt.Println(cur_floor)
-		if cur_floor == floor{
+	//event som venter på at den kommer til en etashe
+	for {
+		cur_floor = driver.Get_floor_sensor_signal()
+		if cur_floor == floor {
 			driver.Set_motor_dir(0)
+			driver.Set_door_open_lamp(1)
+			time.Sleep(1 * time.Second)
+			driver.Set_door_open_lamp(0)
 			return
+			time.Sleep(100 * time.Millisecond)
 		}
 	}
+}
+
+func Update_lights() {
+	var cur_floor int
+	for {
+		cur_floor = driver.Get_floor_sensor_signal()
+		for buttontype := 0; buttontype < 3; buttontype++ {
+			for floor := 0; floor < driver.N_floors; floor++ {
+				if driver.Get_button_signal(buttontype, floor) == 1 {
+					driver.Set_button_lamp(buttontype, floor, 1)
+				}
+				if cur_floor != -1 {
+					driver.Set_floor_indicator(cur_floor)
+				}
+				time.Sleep(1 * time.Millisecond)
+			}
+		}
+	}
+}
+
+func Quit_program(quit chan int) {
+	for {
+		time.Sleep(time.Second)
+		if driver.Get_stop_signal() == 1 {
+			quit <- 1
+		}
+	}
+}
+
+func Run_Elev() {
+
 }
--- src/main/main.go
+++ src/main/main.go
@@ -13,8 +13,6 @@ func main() {
 	driver.Elev_init()
 	go asd.Update_lights()
 	go asd.Go_to_floor(2)
-	time.Sleep(time.Second)
-	go asd.Go_to_floor(1)
 	quit := make(chan int)
 	go asd.Quit_program(quit)
 
